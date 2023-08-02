922edd1978cf044dbe67462e37c97a76ecabd448
Hieu Bui <hieubnd149@gmail.com>
2023-08-02 19:13:40 +0700

    remove null-ls, config solargraph, tmux(+jump)

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -8,14 +8,6 @@ return {
   -- Icon for file icons
   { 'nvim-tree/nvim-web-devicons' },
 
-  {
-    'jose-elias-alvarez/null-ls.nvim',
-    config = function()
-        require("null-ls").setup()
-    end,
-    requires = { "nvim-lua/plenary.nvim" }
-  },
-
   -- Easy Motion
   { 'phaazon/hop.nvim', branch = 'v2' },
 
