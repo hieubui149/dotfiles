1957073e69acee7fccc5614600745157625f8756
Hieu Bui <hieubnd149@gmail.com>
2025-06-17 10:37:29 +0700

    Auto remove blank space

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,6 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- [[ Configure space stop by language ]]
+vim.api.nvim_create_autocmd("BufWritePre", { pattern = "*", command = "%s/\\s\\+$//e" })
